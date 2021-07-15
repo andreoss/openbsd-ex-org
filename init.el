969fadf14d1555c0d162dfb32bc543de95752841
andreoss <andreoss@sdf.org>
2021-07-15 19:32:56 -0400

    Initial

---
+++ init.el
@@ -0,0 +1,4 @@
+(setq org-babel-noweb-wrap-start "«")
+(setq org-babel-noweb-wrap-end "»")
+(setq org-confirm-babel-evaluate nil)
+(progn (org-babel-goto-named-src-block "startup") (org-babel-execute-src-block) (outline-hide-sublevels 1))
